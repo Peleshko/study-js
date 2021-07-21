8d22fb90e3d78625195db616b3dc6ed47f1be3cb
Peleshko <berdyanskpel6@gmail.com>
2021-07-21 17:18:33 +0300

    add files

---
+++ js/variables.js
@@ -0,0 +1,32 @@
+// =====================ПЕРЕНМЕННЫЕ, ОБЬЯВЛЕНИЯ И ИНИЦИАЛИЗАЦИЯ===========================
+console.log(student); // undefined
+var student = "Viki";
+console.log(student);
+
+var count;
+console.log(count); // undefined
+
+count = 1;
+console.log(count);
+count = 2;
+console.log(count);
+
+// console.log(number);
+let number = 30;
+console.log(number);
+
+let age;
+console.log(age); // undefined
+age = 18;
+console.log(age);
+age = 20;
+console.log(age);
+
+
+// console.log(userName);
+const userName = "Vova";
+console.log(userName);
+// userName = ""; // Uncaught TypeError: Assignment to constant variable.
+
+// const user;
+// console.log(user); //Uncaught SyntaxError: Missing initializer in const declaration
