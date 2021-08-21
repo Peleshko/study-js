854f4061073d32cd63c00f3617c811c9374ac373
Peleshko <berdyanskpel6@gmail.com>
2021-08-21 19:29:33 +0300

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,14 +1,9 @@
-function countProps(object) {
-    // Change code below this line
-    let propCount = 0;
-  
-    for (const key in object) {
-      if (object.hasOwnProperty(key)) {
-        propCount += 1;
-      }
-    }
-  
-    return propCount;
-    // Change code above this line
+const atTheOldToad = {
+  // Change code below this line
+  potions: ['Speed potion', 'Dragon breath', 'Stone skin'],
+  getPotions (){
+ const values = Object.values (atTheOldToad); 
   }
+  // Change code above this line
+};
   
